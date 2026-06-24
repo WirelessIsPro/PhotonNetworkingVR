f140110d9e1627230d1582457247d9681793fa67
WirelessIsPro <secretfreetrials@gmail.com>
2026-06-24 08:36:02 -0400

    main files for this
     started working on it yesterday(6/24/26) and thought to release it now, I hope to have this better than PhotonVR which most Gorilla Tag Fangames use

---
+++ NetworkRig.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Photon.Pun;
+
+namespace Networking
+{
+    public class NetworkRig : MonoBehaviourPunCallbacks
+    {
+        public NetworkManager networkManager;
+	public PhotonView pv;
+        public GameObject Head;
+        public GameObject Body;
+        public GameObject LeftHand;
+        public GameObject RightHand;
+
+        void Start()
+        {
+        // now seeing I need a pv check
+         if (pv.IsMine) 
+         {
+            // fucking thing
+            networkManager = FindAnyObjectByType<NetworkManager>();
+            // as soon as this is created do the thing
+            Head.transform.position = networkManager.Head.transform.position;
+            Head.transform.rotation = networkManager.Head.transform.rotation;
+	 }
+        }
+
+        void Update()
+        {
+            Head.transform.position = networkManager.Head.transform.position;
+            Body.transform.rotation = networkManager.Head.transform.rotation;
+            Body.transform.position = networkManager.Body.transform.position;
+            Head.transform.rotation = networkManager.Body.transform.rotation;
+            LeftHand.transform.position = networkManager.LeftHand.transform.position;
+            LeftHand.transform.rotation = networkManager.LeftHand.transform.rotation;
+            RightHand.transform.position = networkManager.RightHand.transform.position;
+            RightHand.transform.rotation = networkManager.RightHand.transform.rotation;
+        }
+    }
+}
